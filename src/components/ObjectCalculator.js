3921f1ac3b3c44c27f0e338f5d04efc42f69162f
Jürgen Baltres <jbaltres@Jurgens-MBP.fritz.box>
2020-07-21 18:09:36 +0200

    made some significant component and style changes

--- src/components/ObjectCalculator.js
+++ src/components/ObjectCalculator.js
@@ -2,50 +2,83 @@ import React from "react";
 import { antwortenFrage1 } from "../api/Data.js";
 import styled from "styled-components"
 export default function ObjectCalculator({
-  posNum,
-  setPosNum,
-  posNum2,
-  setPosNum2,
-  posNum3,
-  setPosNum3
+  stateQuestion1,
+  setStateQuestion1,
+  stateQuestion2,
+  setStateQuestion2,
+  stateQuestion3,
+  setStateQuestion3
 }) 
 
 {
-
-  // Styling Question 
-const QuestionBody = styled.section`
-font-size: 16px;
-color: white;
-padding: 0px 20px
+const Inputstyle = styled.input`
+width: 30px;
+font-size: 1.5em;
+text-align: center;
 `;
-  return (
+  
+const Container = styled.tr`
+display: flex;
+align-items: center;
+border: solid silver 2px;
+background-color: grey;
+margin-bottom: 3px;
+`;
+
+const InputColumn = styled.td`
+display:flex;
+align-items:center;
+justify-content: center;
+width: 200px;
+align-items:center;
+height: 70px;
+`;
+
+const QuestionColumn = styled.div`
+display: flex; 
+align-items: center;
+height: 50px;
+margin: 5px;
+height: 70px;
+`;
+
+return (
     <>
-    <p>
-    <QuestionBody>
-    <>{antwortenFrage1[0]}</>
-    </QuestionBody>
-      
-      <input value={posNum} maxLength="1" onChange={event => setPosNum(event.target.value)} />
-      <span>{posNum}</span>
-      </p>
-      <p>
-        <QuestionBody>{antwortenFrage1[1]}</QuestionBody>
-        <input
-          value={posNum2}
+    <Container>
+      <QuestionColumn>
+        {antwortenFrage1[0]}
+      </QuestionColumn>
+      <InputColumn>
+       <Inputstyle value={stateQuestion1} maxLength="1" onChange={event => setStateQuestion1(event.target.value)} />
+       <span>{stateQuestion1}</span>
+      </InputColumn> 
+    </Container>
+    <Container>
+      <QuestionColumn>
+        {antwortenFrage1[1]}
+      </QuestionColumn>
+      <InputColumn>
+        <Inputstyle
+          value={stateQuestion2}
           maxLength="1"
-          onChange={event => setPosNum2(event.target.value)}
+          onChange={event => setStateQuestion2(event.target.value)}
         />
-        <span>{posNum2}</span>
-      </p>
-      <p>
-      <QuestionBody>{antwortenFrage1[2]}</QuestionBody>
-      <input
-        value={posNum3}
+        <span>{stateQuestion2}</span>
+      </InputColumn>
+    </Container>  
+    <Container>
+      <QuestionColumn>
+       {antwortenFrage1[2]}
+      </QuestionColumn>
+      <InputColumn>
+      <Inputstyle
+        value={stateQuestion3}
         maxLength="1"
-        onChange={event => setPosNum3(event.target.value)}
+        onChange={event => setStateQuestion3(event.target.value)}
       />
-      <span>{posNum3}</span>
-      </p>
+      <span>{stateQuestion3}</span>
+      </InputColumn>
+    </Container>
     </>
   );
 }
